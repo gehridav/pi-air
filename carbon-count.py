16e26aed35d04055a20f8c17d2df1516a3714c11
David Gehrig <gehrig.david@gmail.com>
2017-04-15 23:34:24 +0200

    sensors enabled

--- carbon-count.py
+++ carbon-count.py
@@ -1,11 +1,11 @@
-# import NDIR
+import NDIR
 import time
 import json
 import datetime
 import ConfigParser
 
-#from influxdb import InfluxDBClient
-#import Adafruit_DHT
+from influxdb import InfluxDBClient
+import Adafruit_DHT
 
 
 
@@ -27,14 +27,12 @@ Config.read("config.ini")
 
 # Initialize sensors
 sensor = NDIR.Sensor(Config.getint('Co2Sensor','Address'))
-#sensor.begin()
-#tmpSensor = Adafruit_DHT.DHT11
+sensor.begin()
+tmpSensor = Adafruit_DHT.DHT11
 tmpSensorPin = Config.getint('TempSensor','Gpio')
-
 
 # Initialize InfluxDb client
 #client = InfluxDBClient(Config.get('InfluxDb','Host'), Config.get('InfluxDb','Port'), Config.get('InfluxDb','User'), Config.get('InfluxDb','Password'), Config.get('InfluxDb','Dbname'), True, True)
-
 
 #Initialize current session
 session = Config.get('InfluxDb','Session')
@@ -42,21 +40,18 @@ now = datetime.datetime.now()
 runNo = Config.get('InfluxDb','RunPrefix') + now.strftime("%Y%m%d%H%M")
 
 
-
 try:
     while True:
-        # sensor.measure()
-        humidity, temperature = 10, 11  # Adafruit_DHT.read_retry(tmpSensor, tmpSensorPin)
+        sensor.measure()
+        humidity, temperature = Adafruit_DHT.read_retry(tmpSensor, tmpSensorPin)
 
         utc_datetime = datetime.datetime.utcnow()
         iso = utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
         try:
-            # client.write_points(Measurement(session, runNo, iso, 1234, temperature, humidity).toJSON())
+            #client.write_points(Measurement(session, runNo, iso, sensor.ppm, temperature, humidity).toJSON())
             print("Round:")
-            #measurement = Measurement(session, runNo, iso, 1234, temperature, humidity)
-            print("print")
-            print(Measurement(session, runNo, iso, 1234, temperature, humidity).toJSON())
+            print(Measurement(session, runNo, iso, sensor.ppm, temperature, humidity).toJSON())
         except Exception as ex:
             print ex
 
